dc99267f64431146f7722e909b31b052f8722356
HUBERT <hubertix21@gmail.com>
2023-05-24 16:01:09 +0200

    Dzicz

--- app.py
+++ app.py
@@ -19,7 +19,27 @@ def watch(watch_id):
 
 @app.route('/add_watch', methods=['POST'])
 def add_watch():
-    watch = Watch(**request.form)
+    data = {
+        'kod_produktu': request.form['kod_produktu'],
+        'proba': request.form['proba'],
+        'grawer': 'grawer' in request.form,  # Sprawdzenie czy klucz 'grawer' istnieje w formularzu
+        'dla_kogo': request.form['dla_kogo'],
+        'rodzaj': request.form['rodzaj'],
+        'styl': request.form['styl'],
+        'pochodzenie': request.form['pochodzenie'],
+        'szkielko': request.form['szkielko'],
+        'rodzaj_koperty': request.form['rodzaj_koperty'],
+        'szerokosc_koperty': request.form['szerokosc_koperty'],
+        'grubosc_koperty': request.form['grubosc_koperty'],
+        'typ_paska_bransolety': request.form['typ_paska_bransolety'],
+        'kolor_paska_bransolety': request.form['kolor_paska_bransolety'],
+        'wodoszczelnosc': request.form['wodoszczelnosc'],
+        'mechanizm': request.form['mechanizm'],
+        'gwarancja': request.form['gwarancja'],
+        'kolor_tarczy': request.form['kolor_tarczy']
+    }
+
+    watch = Watch(**data)
     db_session.add(watch)
     db_session.commit()
     return redirect(url_for('index'))
@@ -37,8 +57,11 @@ def delete_watch(watch_id):
 def edit_watch(watch_id):
     watch = Watch.query.get(watch_id)
     if request.method == 'POST':
-        for key, value in request.form.items():
-            setattr(watch, key, value)
+        watch.kod_produktu = request.form.get('kod_produktu')
+        watch.proba = request.form.get('proba')
+        watch.grawer = bool(request.form.get('grawer'))
+        # Update the remaining attributes here
+
         db_session.commit()
         return redirect(url_for('watch', watch_id=watch_id))
     return render_template('edit_watch.html', watch=watch)
